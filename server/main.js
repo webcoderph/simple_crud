94236920d91e2260f976e5be903f132971c9d350
maynard <webcoderph@gmail.com>
2023-02-23 23:58:59 +0800

    zero downtie

--- server/main.js
+++ server/main.js
@@ -1,15 +1,28 @@
-require('dotenv').config()
+require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
-const morgan = require('morgan')
+const morgan = require('morgan');
+const cluster = require('cluster');
+
+const numCPUS = 2;
 
 const app = express();
-const PORT = process.env.PORT || 3030
+const PORT = process.env.PORT || 3030;
 
 app.use(cors());
-app.use(express.json())
-app.use(morgan('combined'))
+app.use(express.json());
+app.use(morgan('combined'));
 app.use('/api', require('./src/config/routes'));
 
+if(cluster.isMaster) {
+	for(let i = 0; i < numCPUS; i++) {
+		cluster.fork();
+	}
 
-app.listen(PORT, () => console.log(`Listening to port ${PORT}`))
+	cluster.on('exit', (worker, code, signal) => {
+		cluster.fork();
+	});
+} else {
+	app.listen(PORT, () => console.log(`Server id ${process.pid} Listening to port ${PORT}`));
+}
+
